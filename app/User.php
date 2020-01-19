f03edc3ea19ca623be4be853ceaeccd5ef30ecc3
yuki ueno <yuki.weaver1204@gmail.com>
2020-01-19 12:12:55 +0900

    heroku 連携1

--- app/User.php
+++ app/User.php
@@ -25,6 +25,7 @@ class User extends Authenticatable
     public function posts() {
       return $this->hasMany('App\Post', 'user_ip_address');
     }
+    
 
     /**
      * The attributes that are mass assignable.
