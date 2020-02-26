ed1febd13656bcd6273e00136ec2c472554b6755
yuki ueno <yuki.weaver1204@gmail.com>
2020-02-27 05:42:30 +0900

    デザイン修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  {{-- <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
   <title>@yield('title')</title>
   <link href="{{ url('/') }}/dist/css/vendor/bootstrap.min.css" rel="stylesheet"><!-- Loading Bootstrap -->
   <link href="{{ url('/') }}/dist/css/flat-ui.min.css" rel="stylesheet"><!-- Loading Flat UI -->
